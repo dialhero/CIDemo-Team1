a9f33a8d32a43509812cc1c8626c1b68d62bd5d4
Patrick Kokholm <patrickkokholm@gmail.com>
2026-04-07 10:46:50 +0200

    Added empty method for Power

--- SimpleCalculator/SimpleCalculator/CalculatorOperations.cs
+++ SimpleCalculator/SimpleCalculator/CalculatorOperations.cs
@@ -23,5 +23,9 @@ namespace SimpleCalculator {
 
             return (double)a / (double)b;
         }
+
+        public double Power(double a, double b) {
+            return 0;
+        }
     }
 }
