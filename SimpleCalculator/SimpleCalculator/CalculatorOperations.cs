91a2c384c5f843b8732845b438bd13f099cbb4cd
Outlashed <130246771+Outlashed@users.noreply.github.com>
2026-04-07 11:09:10 +0200

    Correct arithmetic operations in CalculatorOperations
    Fix incorrect operations in Subtract and Multiply methods.

--- SimpleCalculator/SimpleCalculator/CalculatorOperations.cs
+++ SimpleCalculator/SimpleCalculator/CalculatorOperations.cs
@@ -9,11 +9,11 @@ namespace SimpleCalculator {
         }
 
         public int Subtract(int a, int b) {
-            return a + b;
+            return a - b;
         }
 
         public int Multiply(int a, int b) {
-            return a * b;
+            return a / b;
         }
 
         public double Divide(int a, int b) {
